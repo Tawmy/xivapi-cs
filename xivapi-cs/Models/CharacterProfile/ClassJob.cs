340268290d3cb009d6c7718d114497c6cffc9f9c
Tawmy <mail@tawmy.dev>
2021-07-29 21:59:28 +0200

    Add missing base class implementation

--- xivapi-cs/Models/CharacterProfile/ClassJob.cs
+++ xivapi-cs/Models/CharacterProfile/ClassJob.cs
@@ -1,6 +1,6 @@
 namespace xivapi_cs.Models.CharacterProfile
 {
-    public class ClassJob
+    public class ClassJob: ClassJobBase
     {
         public int ClassID { get; set; }
         public int JobID { get; set; }
--- xivapi-cs/Models/CharacterProfile/ClassJobExtended.cs
+++ xivapi-cs/Models/CharacterProfile/ClassJobExtended.cs
@@ -1,6 +1,7 @@
 namespace xivapi_cs.Models.CharacterProfile
 {
-    public class ClassJobExtended
+    public class ClassJobExtended: ClassJobBase
+
     {
         public Class Class { get; set; }
         public Job Job { get; set; }
