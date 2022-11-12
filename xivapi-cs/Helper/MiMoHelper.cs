1c24f26373caf801cef01cffef4dd4ee3b21314b
Tawmy <mail@tawmy.dev>
2022-11-12 23:48:06 +0100

    MiMoHelper instead of Extension

---
+++ xivapi-cs/Helper/MiMoHelper.cs
@@ -0,0 +1,16 @@
+namespace xivapi_cs.Helper;
+
+public static class MiMoHelper
+{
+    public static decimal GetMinionPercentage(int minions)
+    {
+        var perc = decimal.Divide(minions, Values.MinionsTotal);
+        return decimal.Multiply(perc, 100);
+    }
+
+    public static decimal GetMountPercentage(int mounts)
+    {
+        var perc = decimal.Divide(mounts, Values.MountsTotal);
+        return decimal.Multiply(perc, 100);
+    }
+}
